59d8c03365c314bc0b41a67d93a00addc74af269
Arjun Roy <arroy@eng.ucsd.edu>
2014-05-23 10:30:44 -0700

    Stubs for backend using python flask restful extension.

--- src/backend.py
+++ src/backend.py
@@ -1,61 +1,253 @@
 from flask import Flask, jsonify, make_response
+from flask.ext.restful import Api, Resource, reqparse
 import uuid
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 
-questions = [
-    {
-        'id': u'7d108fd4-9ac3-4a8e-9960-cb271f0de56b',
-        'lid': u'9dd18dcf-5479-47d4-bb79-d3aaf0ebd03f',
-        'title': u'Queue insertion',
-        'body': u'Where does the element get inserted into the queue?',
-        'choices': ['The front', 'The end', 'The middle', 'type error', 'invalid operation'],
-        'correctSet': ['The end'],
-        'rounds': []
-    },
-    {
-        'id': u'4a0bd7a6-0534-424b-ac3f-6bc53e306ce6',
-        'lid': u'61231c5a-6803-4f1a-a49f-ba74d0d86457',
-        'title': u'Stacks vs Queues',
-        'body': u'What is the difference between a stack and a queue',
-        'choices': ['FIFO vs LIFO',
-                    'Stacks are trees',
-                    'Queues are heaps',
-                    'Stacks are randomized but queues are deterministic',
-                    'They are the same'],
-        'correctSet': ['FIFO vs LIFO'],
-        'rounds': []
-    }
-]
+api = Api(app)
 
-@app.route('/classes/<string:class_id>/questions', methods = ['GET'])
-def get_questions(class_id):
-    # we should use class_id, but I'm not bothering for the quick in memory
-    # version
-    # currently always returns all questions
+class CoursesListApi(Resource):
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument('studentId', type=str)
+    self.reqparse.add_argument('instructorId', type=str)
+    super(CoursesListApi, self).__init__()
 
-    print class_id
+  def get(self):
+    '''
+    Return all courses matching the given student and instructor id filters.
+    '''
+    # TODO: Implement
+    return None
 
-    return jsonify({'questions': questions})
+api.add_resource(CoursesListApi, '/courses', endpoint='courses_list')
 
-@app.route('/classes/<string:class_id>/questions/<string:question_id>', methods = ['GET'])
-def get_question(class_id, question_id):
-    # we should use class_id, but I'm not bothering for the quick in memory
-    # version
+class LecturesListApi(Resource):
+  def get(self, courseId):
+    '''
+    Return all lectures for the given course.
+    '''
+    # TODO: Implement.
+    return None
 
-    question = filter(lambda t: t['id'] == question_id, questions)
+api.add_resource(LecturesListApi, '/courses/<string:courseId>/lectures',
+  endpoint='lectures_list')
 
-    if len(question) == 0:
-        abort(404)
+class CourseStudentManifestApi(Resource):
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument('lectureId', type=str)
+    super(CourseStudentManifestApi, self).__init__()
 
-    return jsonify({'questions': question[0]})\
+  def get(self, courseId):
+    '''
+    Return all students enrolled in course (if no lecture specified) or all
+    students attending lecture (if lecture specified).
+    '''
+    # TODO: Implement.
+    return None
 
-# TODO handle PUT, POST, DELETE for questions
+api.add_resource(CourseStudentManifestApi,
+  '/courses/<string:courseId>/students', endpoint='course_student_manifest')
 
-@app.errorhandler(404)
-def not_found(error):
-    return make_response(jsonify( {'error' : 'Not found' } ), 404)
+class UserApi(Resource):
+  def get(self, userId):
+    '''
+    Return user details for specified user.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(UserApi, '/users/<string:userId>', endpoint='user')
+
+class QuestionsApi(Resource):
+  def __init__(self):
+    # Arguments for get()
+    self.getReqparse = reqparse.RequestParser()
+    self.getReqparse.add_argument('lectureId', type=str)
+    self.getReqparse.add_argument('tag', type=str, action='append')
+
+    # Arguments for post()
+    self.postReqparse = reqparse.RequestParser()
+    self.postReqparse.add_argument('lectureId', type=str)
+    self.postReqparse.add_argument('title', type=str, required=True)
+    self.postReqparse.add_argument('body', type=str, required=True)
+    self.postReqparse.add_argument('choices', type=str, required=True,
+      action='append')
+    self.postReqparse.add_argument('correct-choices', type=str, required=True,
+      action='append')
+
+    super(QuestionsApi, self).__init__()
+
+  def get(self, courseId):
+    '''
+    Return all questions, matching the lecture id if provided, and all of the
+    provided tags.
+    '''
+    # TODO: Implement.
+    return None
+
+  def post(self, courseId):
+    '''
+    Add a question with the specified parameters.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(QuestionsApi, '/courses/<string:courseId>/questions')
+
+class EditQuestionApi(Resource):
+  def __init__(self):
+    # Arguments for put()
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument('lectureId', type=str)
+    self.reqparse.add_argument('title', type=str, required=True)
+    self.reqparse.add_argument('body', type=str, required=True)
+    self.reqparse.add_argument('choices', type=str, required=True,
+      action='append')
+    self.reqparse.add_argument('correct-choices', type=str, required=True,
+      action='append')
+    super(EditQuestionApi, self).__init__()
+
+  def put(self):
+    '''
+    Update the specified question with the specified parameters.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(EditQuestionApi,
+  '/courses/<string:courseId>/questions/<string:questionId>',
+  endpoint='edit_question')
+
+class ResponseRoundsApi(Resource):
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument('studentId', type=str)
+
+  def get(self, courseId, questionId):
+    '''
+    Return responses for all rounds for given class and question, optionally
+    filtering on student id.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(ResponseRoundsApi,
+  '/courses/<string:courseId>/questions/<string:questionId>/responses',
+  endpoint='response_rounds')
+
+class ResponseApi(Resource):
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument('responseId', type=str, required=True)
+    super(ResponseApi, self).__init__()
+
+  def post(self, courseId, questionId):
+    '''
+    Given the course and question, note that the current user submitted the
+    specified answer.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(ResponseApi,
+  '/courses/<string:courseId>/question/<string:questionId>/respond',
+  endpoint='response')
+
+class RoundStartApi(Resource):
+  def post(self, courseId, questionId):
+    '''
+    Given the course id and question, start a round of questioning.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(RoundStartApi,
+  '/courses/<string:courseId>/question/<string:questionId>/start',
+  endpoint='start_round')
+
+class RoundEndApi(Resource):
+  def post(self, courseId, questionId):
+    '''
+    Given the course id and question, end the round of questioning.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(RoundEndApi,
+  '/courses/<string:courseId>/question/<string:questionId>/end',
+  endpoint='end_round')
+
+class LecturesApi(Resource):
+  def __init__(self):
+    self.postReqparse = reqparse.RequestParser()
+    self.postReqparse.add_argument('title', type=str)
+    self.postReqparse.add_argument('date', type=int)
+    super(LecturesApi, self).__init__()
+
+  def get(self, courseId):
+    '''
+    Get all lectures for course.
+    '''
+    # TODO: Implement.
+    return None
+
+  def post(self, courseId):
+    '''
+    Add a lecture with specified title and date.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(LecturesApi, '/courses/<string:courseId>/lectures',
+  endpoint='lectures')
+
+class LectureDetailsApi(Resource):
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument('title', type=str)
+    self.reqparse.add_argument('date', type=int)
+    super(LectureDetailsApi, self).__init__()
+
+  def get(self, courseId, lectureId):
+    '''
+    Get lecture details.
+    '''
+    # TODO: Implement.
+    return None
+
+  def put(self, courseId, lectureId):
+    '''
+    Update lecture details.
+    '''
+    # TODO: Implement.
+    return None
+
+api.add_resource(LectureDetailsApi,
+  '/courses/<string:courseId>/lectures/<string:lectureId>',
+  endpoint='lecture_details')
+
+class GradesApi(Resource):
+  def __init__(self):
+    self.reqparse = reqparse.RequestParser()
+    self.reqparse.add_argument('studentId', type=str)
+    self.reqparse.add_argument('lectureId', type=str)
+    self.reqparse.add_argument('questionId', type=str)
+    self.reqparse.add_argument('displayRound', type=str)
+    super(GradesApi, self).__init__()
+
+  def get(self, courseId):
+    '''
+    Return grades for course, filtering optionally on student, lecture, question
+    and response type (first round/last round/all rounds).
+    '''
+    # TODO: Implement
+    return None
+
+api.add_resource(GradesApi, '/courses/<string:courseId>/grades',
+  endpoint='grades')
 
 if __name__ == '__main__':
     app.run()
