e72c2fba3593ce5ea1fcb688cbd2f30428047325
Eric Seidel <gridaphobe@gmail.com>
2014-05-30 14:18:48 -0700

    herokuify

--- src/config.py
+++ src/config.py
@@ -4,7 +4,8 @@ import common
 #db_dir = "/tmp/"
 db_dir = common.script_dir
 basedir = common.script_dir
+sqlite_db = 'sqlite:///' + os.path.join(db_dir, 'gnuclicker.db')
 
 APP_NAME = "gnuclickers"
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(db_dir, 'gnuclicker.db')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', sqlite_db)
 SQLALCHEMY_MIGRATE_REPO = os.path.join(common.script_dir, 'db_repository')
